e0cfb3c6a8509e0f0701f4ab564e1c7371bbac9f
Devin Ledesma <dev.dfledesma@gmail.com>
2025-08-29 17:28:30 -0500

    Up-to-date

--- virtual-taco-stand/src/app/menu/menu.component.spec.ts
+++ virtual-taco-stand/src/app/menu/menu.component.spec.ts
@@ -8,16 +8,25 @@ describe('MenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MenuComponent]
-    })
-    .compileComponents();
+      imports: [MenuComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  /* Create by default when the component is generated
+   * Unit Test 1
+   */
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  /* Unit test 2 */
+  it('should correctly display a list of tacos', () => {
+    const complied = fixture.nativeElement as HTMLElement; // Get the compiled HTML of the component
+    const menuItems = complied.querySelectorAll('.menu-item'); // Get all the menu items
+    expect(menuItems.length).toEqual(component.menu.length); // Check if the number of menu items is equal to the number of items in the menu array
   });
 });
