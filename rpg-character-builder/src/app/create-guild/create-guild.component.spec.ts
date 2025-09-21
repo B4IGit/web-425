f3ab393edb817a9d3220ef747bbcf1f18040b6d7
Devin Ledesma <dev.dfledesma@gmail.com>
2025-09-21 16:08:42 -0500

    Added unit test for form validation

--- rpg-character-builder/src/app/create-guild/create-guild.component.spec.ts
+++ rpg-character-builder/src/app/create-guild/create-guild.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CreateGuildComponent } from './create-guild.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('CreateGuildComponent', () => {
   let component: CreateGuildComponent;
@@ -8,9 +9,8 @@ describe('CreateGuildComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CreateGuildComponent]
-    })
-    .compileComponents();
+      imports: [CreateGuildComponent, ReactiveFormsModule, FormsModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CreateGuildComponent);
     component = fixture.componentInstance;
@@ -19,5 +19,43 @@ describe('CreateGuildComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be invalid when empty', () => {
+    expect(component.guildForm.valid).toBeFalsy();
+  });
+
+  it('form should be valid when filled correctly', () => {
+    component.guildForm.controls['guildName'].setValue('Test Guild');
+    component.guildForm.controls['guildDescription'].setValue(
+      'Test Description',
+    );
+    component.guildForm.controls['guildType'].setValue('Competitive');
+    component.guildForm.controls['guildAcceptTerms'].setValue(true);
+    component.guildForm.controls['guildNotificationPreference'].setValue(
+      'Email',
+    );
+
+    expect(component.guildForm.valid).toBeTruthy();
+  });
+
+  it('should display validFormSubmission when valid form data is submitted ', () => {
+    spyOn(component, 'validFormSubmission');
+
+    component.guildForm.controls['guildName'].setValue('Test Guild');
+    component.guildForm.controls['guildDescription'].setValue(
+      'Test Description',
+    );
+    component.guildForm.controls['guildType'].setValue('Competitive');
+    component.guildForm.controls['guildAcceptTerms'].setValue(true);
+    component.guildForm.controls['guildNotificationPreference'].setValue(
+      'Email',
+    );
+    fixture.detectChanges();
+
+    const validForm = fixture.debugElement.query(By.css('form'));
+    validForm.triggerEventHandler('ngSubmit', null);
+
+    expect(component.validFormSubmission).toHaveBeenCalled();
   });
 });
